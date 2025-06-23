88c6eb512098be58cf58277a42f761769857c5ab
artemis_h <tranphamminhhung65@gmail.com>
2025-06-23 22:56:37 +0700

    feat(members): update member card layout

--- src/components/members-page/index.tsx
+++ src/components/members-page/index.tsx
@@ -48,7 +48,7 @@ export default function Members() {
                 </div>
 
                 {/* Profile Cards Grid */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                   {group.people.map((person) => (
                     <ProfileCard key={person.id} person={person} />
                   ))}
--- src/components/members-page/member-card.tsx
+++ src/components/members-page/member-card.tsx
@@ -42,11 +42,12 @@ export default function ProfileCard({ person }: ProfileCardProps) {
   console.log("Image source:", imageSrc);
   return (
     <div
-      className="hover:shadow-md transition-shadow duration-200 
-    w-fit
+      className="hover:shadow-2xl 
+    w-fit hover:scale-105 transition-all duration-300 ease-in-out hover:z-30 group
     bg-white relative profile-container"
     >
-      <div className="flex flex-col items-center text-center group relative z-10 overflow-hidden">
+      {/* Main card */}
+      <div className="flex flex-col items-center text-center z-10 overflow-hidden">
         {/* Profile image */}
         <div className="relative w-[260px] h-[300px] cursor-pointer">
           <Image
@@ -58,17 +59,10 @@ export default function ProfileCard({ person }: ProfileCardProps) {
              "
           />
 
-          {/* White circle decorator */}
-          {/* <div
-            className=" z-50 absolute top-[35%] left-1/2 -translate-y-1/2 -translate-x-1/2 
-            group-hover:size-[220px] size-[400px] duration-300 ease-in-out 
-            rounded-full bg-transparent border border-white"
-          ></div> */}
-
           {/* Darken overlay */}
           <div
             className="w-full h-full absolute top-0 left-0 z-10 
-            opacity-100  duration-100 ease-in-out
+            opacity-100 duration-100 ease-in-out
           bg-gradient-to-t from-[#003153]/90 via-[#003153]/10 to-transparent
           "
           >
@@ -101,27 +95,29 @@ export default function ProfileCard({ person }: ProfileCardProps) {
               />
             </Link>
           </div> */}
-        </div>
-        {/* Profile info */}
-        <div
-          className="relative sm:left-2 flex flex-col items-start justify-end z-[99] 
-        group-hover:scale-105 group-hover:bottom-4 group-hover:left-4 transition-all duration-300 ease-in-out
-        "
-        >
-          {/* Name */}
-          <h3
-            className="text-lg font-semibold text-gray-900 mb-2 leading-none 
-          group-hover:text-white transition-all duration-300 ease-in-out
-          "
-          >
-            {person.name}
-          </h3>
-          {/* Position */}
+
+          {/* Profile info */}
           <div
-            className="text-sm text-gray-600 mb-1 leading-none 
-          group-hover:text-gray-100 transition-all duration-300 ease-in-out"
+            className="absolute w-full bottom-0 left-0 sm:pl-2 flex flex-col items-start text-left z-[99] py-2 bg-white
+        "
           >
-            {person.interest}
+            {/* Name */}
+            <h3
+              className="text-lg font-semibold text-gray-900 mb-2 leading-none 
+          "
+            >
+              {person.name}
+            </h3>
+            {/* Position */}
+            {person.interest && (
+              <div
+                className="text-sm text-gray-600 mb-1 leading-none overflow-hidden transition-all duration-300 ease-in-out
+               line-clamp-2 h-0 group-hover:h-[30px] 
+           "
+              >
+                {person.interest}
+              </div>
+            )}
           </div>
         </div>
       </div>
